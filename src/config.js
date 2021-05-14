4f593eae147dc4c34148881c8016ed7127f5a7cf
SamArsanios <samson.kibrom@hotmail.com>
2021-05-15 01:12:45 +0300

    To Refactor

---
+++ src/config.js
@@ -0,0 +1,33 @@
+// // import Phaser from 'phaser';
+// import 'phaser';
+
+// export default {
+//     type: Phaser.AUTO,
+//     parent: "game",
+//     width: 1280,
+//     height: 720,
+//     backgroundColor: "#5DACD8",
+//     scene: {
+//         init: initScene,
+//         preload: preloadScene,
+//         create: createScene,
+//         update: updateScene
+//     }
+// };
+
+// function initScene() {
+
+// }
+
+// function preloadScene() {
+//     this.load.image("clouds-blue", "blue.png")
+// }
+
+// function createScene() {
+//     var cloudsBlue = this.add.sprite(640, 360, "clouds-blue")
+// }
+
+// function updateScene() {
+
+// }
+
---
+++ src/index.js
@@ -0,0 +1,18 @@
+// import 'phaser';
+// import config from './config';
+
+// class Game extends Phaser.Game {
+//     constructor() {
+//         super(config);
+//     }
+// }
+// window.game = new Game()
+
+// // window.game = new Game(phaserConfig)
+// // const preloadScene = () => {
+
+// // }
+
+// const preload = () => {
+//     this.load.image("clouds-blue", "blue-clouds.png")
+// }
