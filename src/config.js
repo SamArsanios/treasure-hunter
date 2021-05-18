9e1d958f13d86ad55f0e5801d07120cc22255bf0
SamArsanios <samson.kibrom@hotmail.com>
2021-05-18 12:07:05 +0300

    Add game structure and scenes to config

--- src/config.js
+++ src/config.js
@@ -1,32 +1,19 @@
-// // import Phaser from 'phaser';
-// import 'phaser';
+// import Phaser from 'phaser';
+import 'phaser';
+import GameplayScene from "./Scenes/gameplay-scene"
+import GameoverScene from "./Scenes/gameover-scene"
 
-// export default {
-//     type: Phaser.AUTO,
-//     parent: "game",
-//     width: 1280,
-//     height: 720,
-//     backgroundColor: "#5DACD8",
-//     scene: {
-//         init: initScene,
-//         preload: preloadScene,
-//         create: createScene,
-//         update: updateScene
-//     }
-// };
-
-// function initScene() {
-
-// }
-
-// function preloadScene() {
-//     this.load.image("clouds-blue", "blue.png")
-// }
-
-// function createScene() {
-//     var cloudsBlue = this.add.sprite(640, 360, "clouds-blue")
-// }
-
-// function updateScene() {
-
-// }
+export default {
+    type: Phaser.AUTO,
+    parent: "game",
+    width: 1280,
+    height: 600,
+    backgroundColor: "#5DACD8",
+    physics: {
+        default: "arcade",
+        arcade: {
+            debug: false
+        }
+    },
+    scene: [GameplayScene, GameoverScene]
+}
