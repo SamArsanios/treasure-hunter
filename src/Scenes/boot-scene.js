37cff59eb915d1327dd565cdf8d7579be8cb9a06
SamArsanios <samson.kibrom@hotmail.com>
2021-05-18 18:24:26 +0300

    Add Boot  Scene

---
+++ src/Scenes/boot-scene.js
@@ -0,0 +1,15 @@
+import 'phaser';
+
+export default class BootScene extends Phaser.Scene {
+    constructor() {
+        super('Boot');
+    }
+
+    preload() {
+        this.load.image('boot', 'assets/boot-image1.jpg');
+    }
+
+    create() {
+        this.scene.start('Preloader');
+    }
+};
