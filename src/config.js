efa2d1b2ca574981783eaf10b8d26493e10b7e50
SamArsanios <samson.kibrom@hotmail.com>
2021-05-18 22:12:47 +0300

    Add bundled js, modify start scenes

--- src/config.js
+++ src/config.js
@@ -1,8 +1,10 @@
 // import Phaser from 'phaser';
 import 'phaser';
 import BootScene from "./Scenes/boot-scene"
-import TitleScene from "./Scenes/title-scene"
 import PreloaderScene from "./Scenes/preloader-scene"
+import TitleScene from "./Scenes/title-scene"
+import OptionsScene from './Scenes/options-scene';
+import CreditsScene from './Scenes/credits-scene';
 import GameplayScene from "./Scenes/gameplay-scene"
 import GameoverScene from "./Scenes/gameover-scene"
 
@@ -11,12 +13,14 @@ export default {
     parent: "game",
     width: 1280,
     height: 600,
-    backgroundColor: "#5DACD8",
+    // backgroundColor: "#5DACD8",
     physics: {
         default: "arcade",
         arcade: {
             debug: false
         }
     },
-    scene: [BootScene, TitleScene, PreloaderScene, GameplayScene, GameoverScene]
+    scene: [ GameplayScene, GameoverScene]
 }
+
+// BootScene, TitleScene, PreloaderScene, OptionsScene, CreditsScene
--- src/index.js
+++ src/index.js
@@ -1,16 +1,27 @@
 import 'phaser';
 import config from './config';
+
+import BootScene from "./Scenes/boot-scene";
+import PreloaderScene from "./Scenes/preloader-scene"
+import TitleScene from "./Scenes/title-scene"
+import OptionsScene from "./Scenes/options-scene"
+import CreditsScene from "./Scenes/credits-scene"
 import GameplayScene from "./Scenes/gameplay-scene"
 import GameoverScene from "./Scenes/gameover-scene"
-import BootScene from "./Scenes/boot-scene";
-
+import Model from './Model';
 class Game extends Phaser.Game {
     constructor() {
         super(config);
-        // this.scene.add('Game', GameplayScene);
-        // this.scene.add('Game', GameoverScene);
+        const model = new Model();
+        this.globals = { model, bgMusic: null };
         // this.scene.add('Boot', BootScene);
-        this.scene.start('Game');
+        // this.scene.add('Preloader', PreloaderScene);
+        // this.scene.add('Title', TitleScene)
+        // this.scene.add('Options', OptionsScene);
+        // this.scene.add('Credits', CreditsScene);
+        // this.scene.add('Game', GameplayScene);
+        // this.scene.add('Gameover', GameoverScene);
+        this.scene.start('Boot');
     }
 }
 window.game = new Game()
