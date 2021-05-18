1bbc7ff5ed831613cc5b14a8e8e8ab8adfb35e4b
SamArsanios <samson.kibrom@hotmail.com>
2021-05-18 12:28:31 +0300

    Add Score and Retry on gameover

--- src/Scenes/gameover-scene.js
+++ src/Scenes/gameover-scene.js
@@ -13,6 +13,32 @@ export default class GameoverScene extends Phaser.Scene {
     }
 
     create() {
-        this.add.text(640, 300, "GAME OVER").setOrigin(0.5);
+        this.add.text(640, 200, "GAME OVER", {
+            fontSize: 60,
+            color: "#000",
+            fontStyle: "bold",
+            backgroundColor: "#FFF",
+            padding: 10
+        }).setOrigin(0.5);
+
+        this.add.text(
+            10, 10,
+            "SCORE: 0",
+            {
+                fontSize: 30,
+                color: "#000000",
+                fontStyle: "bold",
+                // backgroundColor: "#eeeeee",
+                // padding: 0
+            }
+        );
+
+        this.retryButton = this.add.text(1125, 475, "RETRY", {
+            fontSize: 40,
+            color: "#000",
+            fontStyle: "bold",
+            backgroundColor: "#FFF",
+            // padding: 10
+        });
     }
 }
