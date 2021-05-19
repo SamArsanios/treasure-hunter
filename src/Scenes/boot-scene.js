43009ce31e261273c9578681ca60ae648580c2a7
SamArsanios <samson.kibrom@hotmail.com>
2021-05-19 11:10:25 +0300

    Add blue background on load

--- src/Scenes/boot-scene.js
+++ src/Scenes/boot-scene.js
@@ -6,7 +6,7 @@ export default class BootScene extends Phaser.Scene {
     }
 
     preload() {
-        this.load.image('boot', 'assets/boot-image1.jpg');
+        this.load.image('boot', 'assets/boot-image2.jpg');
     }
 
     create() {
