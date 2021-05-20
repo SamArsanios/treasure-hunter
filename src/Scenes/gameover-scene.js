36bc7bb781133fd68434345c8d359881947eefe1
SamArsanios <samson.kibrom@hotmail.com>
2021-05-20 16:31:24 +0300

    Add Menu Button to Game Over scene

--- src/Scenes/gameover-scene.js
+++ src/Scenes/gameover-scene.js
@@ -1,5 +1,5 @@
 import Phaser from 'phaser';
-
+import Button from '../Objects/Button'
 export default class GameoverScene extends Phaser.Scene {
   constructor() {
     super('GameoverScene');
@@ -15,28 +15,30 @@ export default class GameoverScene extends Phaser.Scene {
       color: '#000',
       fontStyle: 'bold',
       padding: 10,
-    }).setOrigin(0.5);
+    }).setOrigin(0.5, 0.5);
 
     this.add.text(
-      560, -100,
+      640, 30,
       `SCORE: ${this.score}`,
       {
         fontSize: 45,
         color: '#000000',
         fontStyle: 'bold',
       },
-    );
+    ).setOrigin(0.5, 0.5);
 
-    this.retryButton = this.add.text(580, 170, 'RETRY', {
+    this.retryButton = this.add.text(640, 300, 'RETRY', {
       fontSize: 45,
       color: '#000',
       fontStyle: 'bold',
-    });
+    }).setOrigin(0.5, 0.5);
 
     this.retryButton.setInteractive();
 
     this.retryButton.on('pointerdown', () => {
       this.scene.start('Game');
     });
+
+    this.menuButton = new Button(this, 640, 500, 'blueButton1', 'blueButton2', 'Menu', 'Title');
   }
 }
